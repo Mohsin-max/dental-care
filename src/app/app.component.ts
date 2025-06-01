f5d727e04467e6c1326db25aef3298d4984dcad1
Mohsin Ali <javascript492@gmail.com>
2025-06-01 12:52:20 -0700

    new 2 sections added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,13 @@ import { AboutComponent } from "./components/about/about.component";
 import { ScrollingTextComponent } from "./components/scrolling-text/scrolling-text.component";
 import { OurServicesComponent } from "./components/our-services/our-services.component";
 import { WhyUsComponent } from "./components/why-us/why-us.component";
+import { CaseStoriesComponent } from "./components/case-stories/case-stories.component";
+import { HowItWorksComponent } from "./components/how-it-works/how-it-works.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TopHeaderComponent, HeaderComponent, HeroComponent, AboutComponent, ScrollingTextComponent, OurServicesComponent, WhyUsComponent],
+  imports: [RouterOutlet, TopHeaderComponent, HeaderComponent, HeroComponent, AboutComponent, ScrollingTextComponent, OurServicesComponent, WhyUsComponent, CaseStoriesComponent, HowItWorksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/case-stories/case-stories.component.ts
+++ src/app/components/case-stories/case-stories.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from "../../shared/button/button.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-case-stories',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent,CommonModule],
   templateUrl: './case-stories.component.html',
   styleUrl: './case-stories.component.css'
 })
 export class CaseStoriesComponent {
+
+  cardsDetails:any[]=[
+
+    {
+      image:"story1.jpg",
+      heading:"A Brighter Tomorrow: Sarah's Whitening Journey",
+      tag1:"2024",
+      tag2:"Teeth Whitening",
+      tag3:"Dental Care",
+    },
+
+    {
+      image:"story2.jpg",
+      heading:"A Beautiful Transformation: Olivia's Braces Journey",
+      tag1:"2024",
+      tag2:"Braces Treatment",
+      tag3:"Dental Care",
+    }
+
+  ]
 
 }
